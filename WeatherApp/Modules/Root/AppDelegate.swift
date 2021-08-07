f52bbe3950a3010cec64f75837be40e7abc8e323
apple <Abanoub.Habib@warbabank.com>
2021-08-07 16:02:59 +0200

    setup window

--- WeatherApp/Modules/Root/AppDelegate.swift
+++ WeatherApp/Modules/Root/AppDelegate.swift
@@ -10,11 +10,22 @@ import CoreData
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    // MARK: - Attributes
+    
+    var window: UIWindow?
+    
+    // MARK: - Application Life Cycle
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        setupWindow()
         return true
     }
 
+    // MARK: - Methods
+    
+    func setupWindow() {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+    }
     // MARK: - Core Data stack
 
     lazy var persistentContainer: NSPersistentContainer = {
