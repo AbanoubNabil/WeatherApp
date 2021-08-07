f3df2c1009edc435ce8b818b2632f9ff628af822
apple <Abanoub.Habib@warbabank.com>
2021-08-07 15:31:03 +0200

    add Environment add API Service

---
+++ WeatherApp/Core/APIService/APIService.swift
@@ -0,0 +1,69 @@
+//
+//  APIService.swift
+//  WeatherApp
+//
+//  Created by Abanoub Nabil on 8/7/21.
+//
+
+import Moya
+
+class RemoteServiceType: EnvironmentProtocol {
+    // MARK: - Attributes
+    var isLive: Bool { currentEnvironment != .test }
+    static var shared = RemoteServiceType()
+    
+    // MARK: - Init
+    private init() {}
+}
+
+class APIService<T: TargetType> {
+    
+    // MARK: - Attributes
+    private let provider: MoyaProvider<T>
+    
+    private let jsonDataFormatter = { (_ data: Data) -> String  in
+        do {
+            let dataAsJSON = try JSONSerialization.jsonObject(with: data)
+            let prettyData = try JSONSerialization.data(withJSONObject: dataAsJSON, options: .prettyPrinted)
+            return String(data: prettyData, encoding: .utf8) ?? ""
+        } catch {
+            return String(data: data, encoding: .utf8) ?? ""
+        }
+    }
+    
+    private let endpointClosure = { (target: T) -> Endpoint in
+       let defaultEndpoint = MoyaProvider.defaultEndpointMapping(for: target)
+       var request = try? defaultEndpoint.urlRequest()
+       request?.cachePolicy = .useProtocolCachePolicy
+       return defaultEndpoint
+   }
+    
+    // MARK: - Init
+    init() {
+        let stubClosure = RemoteServiceType.shared.isLive ? MoyaProvider<T>.neverStub : MoyaProvider<T>.immediatelyStub
+        provider = MoyaProvider<T>(endpointClosure: endpointClosure,
+                                   stubClosure: stubClosure,
+                                   session: Session.default,
+                                   plugins: [NetworkLoggerPlugin(configuration: .init(formatter: .init( requestData: jsonDataFormatter, responseData: jsonDataFormatter), logOptions: .verbose))])
+        provider.session.sessionConfiguration.timeoutIntervalForRequest = 20
+        provider.session.sessionConfiguration.timeoutIntervalForResource = 20
+    }
+    
+    // MARK: - Methods
+    func request<C: Codable>(target: T, objType: C.Type, completionHandler:  @escaping (_ result: Result<C, Error>) -> Void) {
+        provider.request(target) { (result) in
+            switch result {
+            case .success(let response):
+                do {
+                    let successResponse = try response.filterSuccessfulStatusCodes()
+                    let object = try JSONDecoder().decode(objType, from: successResponse.data)
+                    completionHandler(.success(object))
+                } catch let error {
+                    completionHandler(.failure(error))
+                }
+            case .failure(let error):
+                completionHandler(.failure(error))
+            }
+        }
+    }
+}
